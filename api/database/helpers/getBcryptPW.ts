f8d07d3af8a04e2722419b8035993c7fb658cbd4
Zak <zak.johansson@gmail.com>
2023-03-30 16:55:43 -0700

    create user seed

---
+++ api/database/helpers/getBcryptPW.ts
@@ -0,0 +1,9 @@
+//import BCrypt to create hashed password for user seed database
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const plainTextPW = 'password';
+
+bcrypt.hash(plainTextPW, saltRounds).then((result) => {
+  console.log(result);
+});
--- api/database/seeds/01_users.sql
+++ api/database/seeds/01_users.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (username, email, password)
-VALUES("Zensei", "zak.johansson@gmail.com", )
+VALUES("Zensei", "zak.johansson@gmail.com", '$2b$10$EZS0OzNhdriB2xreqHQNfOVA6xMINwzIUuHA3LP5oVZMdGBGRU32W' )
