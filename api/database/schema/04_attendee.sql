c2a78a238b054c0b4733e0e99c481143d926dc61
Zak <zak.johansson@gmail.com>
2023-03-30 16:22:00 -0700

    create attendee schema

---
+++ api/database/schema/04_attendee.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS attendee CASCADE;
+CREATE TABLE attendee (
+  id PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  event_id INTEGER REFERENCES event(id),
+  status VARCHAR(30) NOT NULL,
+  created_at TIMESTAMP default now(),
+  updated_at TIMESTAMP
+)
