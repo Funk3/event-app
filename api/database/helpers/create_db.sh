57cf5aacf008df3507c43651fad832fe84f0adf1
Zak <zak.johansson@gmail.com>
2023-04-03 14:50:36 -0700

    add database script

---
+++ api/database/helpers/create_db.sh
@@ -0,0 +1,9 @@
+# Set the database name and credentials
+DB_NAME="event_app"
+DB_USER="dad"
+DB_PASS="dadrock"
+
+# Create the database and grant permissions to the user
+psql -c "CREATE DATABASE $DB_NAME;"
+psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';"
+psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
