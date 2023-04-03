27e2e22b3a6f1d82756ddf8c5c5fb405387be6cc
Zak <zak.johansson@gmail.com>
2023-04-03 14:06:22 -0700

    add place table

---
+++ api/database/schema/05_place.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS category CASCADE;
+CREATE TABLE place (
+  id PRIMARY KEY NOT NULL,
+  name UNIQUE NOT NULL,
+  description TEXT NOT NULL,
+  category_id INTEGER REFERENCES category(id),
+  owner_id INTEGER REFERENCES users(id),
+  address TEXT NOT NULL,
+  latitude VARCHAR(40) NOT NULL,
+  longitude VARCHAR(40) NOT NULL
+);
