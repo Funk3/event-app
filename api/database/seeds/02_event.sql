fe681fe5a39c21cc6575478204b4d889390f80d1
Zak <zak.johansson@gmail.com>
2023-04-03 14:31:51 -0700

    add seed data for place

--- api/database/seeds/02_event.sql
+++ api/database/seeds/02_event.sql
@@ -1,4 +1,4 @@
-INSERT INTO category (title, description, category_id, start_date_time, end_date_time, latitude, longitude);
+INSERT INTO category (title, description, category_id, start_date_time, end_date_time,);
 VALUES("infinity", "A time to play infinity!", "3","2023-04-21 19:00:00", "2023-04-21 21:00:00", ),
 ('Warhammer 40k', 'a time to play warhammer!', '1',"2023-4-15 19:00:00", "2023-04-21 21:00:00", ),
 ("Age of Sigmar", 'A time to play age of sigmar!', '2', '2023-04-28 19:00:00', '2023-04-28 21:00:00', '')
---
+++ api/database/seeds/05_place.sql
@@ -0,0 +1,5 @@
+INSERT INTO place (name, description, category_id, address, latitude, longitude);
+VALUES ('The Connection Games & Hobbies', "Game Store", "3,4", '2945 Renfrew St, Vancouver, BC V5M 3K5', '49.2601407276203', '-123.0440414949794'),
+('Strategies Games & Hobbies', 'Yet another game store', '1,2', '3878 Main St, Vancouver, BC V5V 3N9', '49.25063528858372', '-123.1007373598636'),
+('The Spire Games', 'Private club for gaming', '1, 2, 3', '4338 Main St #104, Vancouver, BC V5V 3P9' '49.24652055097584', '-123.10102350619884'),
+('Rain City Games', 'A board game shop that also does magic!', '4', '88 W Pender St Unit 1069, Vancouver, BC V6B 6N9', '49.28064324186129', '-123.10707706618037')
