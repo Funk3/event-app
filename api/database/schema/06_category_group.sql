f96cd429d95e3f5d685b88620ceefc472433c755
Zak <zak.johansson@gmail.com>
2023-04-03 20:20:23 -0700

    add category group seed data

--- api/database/schema/06_category_group.sql
+++ api/database/schema/06_category_group.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS category_group CASCADE;
 CREATE TABLE category_group (
-  user_id INTEGER REFERENCES users(id),
+  place_id INTEGER REFERENCES place(id),
   category_id INTEGER REFERENCES category(id)
 );
---
+++ api/database/seeds/05_category_group.sql
@@ -0,0 +1,2 @@
+INSERT INTO category_group (place_id, category_id)
+VALUES (1, 3), (1, 4), (2, 1), (2, 2), (3, 1), (3, 2), (3, 3), (4, 4);
