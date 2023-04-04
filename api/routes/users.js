b0021513a6fd7eb2c34196233c08173a0efaabe2
Zak <zak.johansson@gmail.com>
2023-04-04 11:36:33 -0700

    add login route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express'
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+import { getUserByEmail, addUser } from '../database/queries/users'
+
+router.post('/login', async (req, res) => {
+  const user = await getUserByEmail(req.body.email)
+  res.json(user);
 });
 
 module.exports = router;
